7bf954aef7b6cfb8a37d69a0a13423c2ebe87221
VitalyLut <lut1910@gmail.com>
2026-04-06 13:19:28 +0300

    add

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,9 +2,5 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(num) {
-    if (num % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return num % 2 === 0;
 }

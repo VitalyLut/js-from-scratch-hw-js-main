70ec0110dc0ef55488eea86f472dea47650957f9
VitalyLut <lut1910@gmail.com>
2026-04-08 16:29:42 +0300

    add

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,14 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+    let uniqueArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let currentElement = array[i];
+
+        if (!includesElement(uniqueArray, currentElement)) {
+            uniqueArray.push(currentElement);
+        }
+    }
+    return uniqueArray;
+}
